758ed3d7c153578730d5ea99e77f7585a240cb32
kimpublic <minjoongkim98@naver.com>
2025-07-17 01:41:11 +0900

    login done

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -5,10 +5,6 @@ import dataaccess.DataAccessOnMemory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import service.RegisterRequest;
-import service.RegisterResult;
-import service.UserService;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
@@ -21,6 +17,7 @@ public class UserServiceTest {
         service = new UserService(dataAccessObject);
     }
 
+    // 가입 테스트
     @Test
     void registerPositive() throws DataAccessException {
         RegisterRequest request = new RegisterRequest("minjoong", "password", "minjoongKim98@naver.com");
@@ -29,16 +26,16 @@ public class UserServiceTest {
         assertNotNull(response.authToken());
     }
 
-    // 잘못된 요청
+    // 가입-잘못된 요청
     @Test
-    void registerBadRequest() throws IllegalArgumentException {
-        RegisterRequest bad = new RegisterRequest("","","");
+    void registerBadRequest() {
+        RegisterRequest badRegister = new RegisterRequest("","","");
         assertThrows(IllegalArgumentException.class, () -> {
-            service.register(bad);
+            service.register(badRegister);
         });
     }
 
-    // already taken 케이스
+    // 가입-already taken 케이스
     @Test
     void registerAlreadyTaken() throws DataAccessException {
         RegisterRequest request = new RegisterRequest("minjoong", "password", "minjoongKim98@naver.com");
@@ -49,7 +46,54 @@ public class UserServiceTest {
         } catch (DataAccessException e) {
             assertEquals("already taken", e.getMessage());
         }
-
     }
+
+    // login 테스트
+    @Test
+    void loginPositive() throws DataAccessException {
+        RegisterRequest registerRequest = new RegisterRequest("minjoong", "password", "minjoongkim98@naver.com");
+        service.register(registerRequest);
+
+        LoginRequest loginRequest = new LoginRequest("minjoong","password");
+        LoginResult loginResult = service.login(loginRequest);
+        assertEquals("minjoong", loginResult.username());
+        assertNotNull(loginResult.authToken());
+    }
+
+    // 로그인-잘못된 요청
+    @Test
+    void loginBadRequest() throws IllegalArgumentException {
+        LoginRequest badLogin = new LoginRequest("","");
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.login(badLogin);
+        });
+    }
+
+    // 로그인-없는 아이디
+    @Test
+    void loginWrongUsername() throws DataAccessException {
+        LoginRequest wrongUsername = new LoginRequest("not_valid_username", "hehe");
+        try {
+            service.login(wrongUsername);
+            fail("test failed");
+        } catch (DataAccessException e) {
+            assertEquals("unauthorized", e.getMessage());
+        }
+    }
+
+    // 로그인-잘못된 패스워드
+    @Test
+    void loginWrongPassword() throws DataAccessException {
+        RegisterRequest registerWrongPassword = new RegisterRequest("minjoong", "valid", "minjoongkim98@naver.com");
+        service.register(registerWrongPassword);
+        LoginRequest loginWrongPassword = new LoginRequest("minjoong", "not-valid");
+        try {
+            LoginResult loginResult = service.login(loginWrongPassword);
+            fail("test failed");
+        } catch (DataAccessException e) {
+            assertEquals("unauthorized", e.getMessage());
+        }
+    }
+
 
 }
