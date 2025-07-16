01724862c309a2a336ed2a98302e1d6efcc42bfa
kimpublic <minjoongkim98@naver.com>
2025-07-16 20:45:00 +0900

    clear test done, BOM problem needed to be watched

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,1 +1,26 @@
-ÿþ
+//clear test
+package service;
+
+import dataaccess.DataAccessException;
+import dataaccess.DataAccessOnMemory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClearServiceTest {
+    private ClearService service;
+    private DataAccessOnMemory dataAccessObject;
+
+    @BeforeEach
+    void testSetUp() {
+        dataAccessObject = new DataAccessOnMemory();
+        service = new ClearService(dataAccessObject);
+    }
+
+    @Test
+    void clearShouldNotThrow() {
+        assertDoesNotThrow(() -> service.clear());
+    }
+}
