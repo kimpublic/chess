74f032eba5c04bda8501a8f8f5cfede9eb35b7b7
kimpublic <minjoongkim98@naver.com>
2025-07-29 04:51:25 +0900

    still working on console, need to do list / create / join / watch cmd and chess board creation, and make unit tests

--- client/src/main/java/client/Console.java
+++ client/src/main/java/client/Console.java
@@ -10,6 +10,7 @@ public class Console {
     private final Scanner scanner = new Scanner(System.in);
 
     private boolean loggedIn = false;
+    private boolean gameMode = false;
     private String cmd = "";
 
     public Console(ServerFacade facade) {
@@ -23,7 +24,7 @@ public class Console {
             System.out.println("Register as a new user: \"register\" <USERNAME> <PASSWORD> <EMAIL>");
             System.out.println("Exit the program: \"quit\"");
             System.out.println("Print this action options page: \"help\"");
-        } else {
+        } else if (!gameMode) {
             System.out.println("Options:");
             System.out.println("List current games: \"list\"");
             System.out.println("Create a new game: \"create\" <GAME NAME>");
@@ -32,6 +33,8 @@ public class Console {
             System.out.println("Logout: \"logout\"");
             System.out.println("Exit the program: \"quit\"");
             System.out.println("Print this action options page: \"help\"");
+        } else {
+
         }
     }
 
@@ -44,7 +47,7 @@ public class Console {
             String line = scanner.nextLine().trim();
             String[] parsed = line.split(" ", 2);
             cmd = parsed[0];
-            String arguments = parsed[1];
+
             switch (cmd) {
                 case "quit": {
                     System.out.println("Hope you had a fun time with us. Good bye and see you next time!");
@@ -56,32 +59,52 @@ public class Console {
                 }
                 case "register": {
                     try {
+                        String arguments = parsed[1];
                         String[] argumentsParsed = arguments.split(" ");
-                        if (argumentsParsed.length < 4) {
+                        if (argumentsParsed.length < 3) {
                             System.out.println("Usage: \"register\" <USERNAME> <PASSWORD> <EMAIL>");
                             break;
                         }
-                        facade.register(argumentsParsed[1], argumentsParsed[2], argumentsParsed[3]);
+                        facade.register(argumentsParsed[0], argumentsParsed[1], argumentsParsed[2]);
                         loggedIn = true;
-                        System.out.println(">> Registered! You are not logged in.");
+                        System.out.println(">> Registered! You are now logged in.");
+                        break;
                     } catch (Exception e) {
                         System.out.println("Error: " + e.getMessage());
                     }
                 }
                 case "login": {
                     try {
+                        String arguments = parsed[1];
                         String[] argumentsParsed = arguments.split(" ");
-                        if (argumentsParsed.length < 3) {
+                        if (argumentsParsed.length < 2) {
                             System.out.println("Usage: \"login\" <USERNAME> <PASSWORD>");
                             break;
                         }
-                        facade.login(argumentsParsed[1], argumentsParsed[2]);
+                        facade.login(argumentsParsed[0], argumentsParsed[1]);
                         loggedIn = true;
                         System.out.println(">> You are now logged in.");
+                        help();
+                        break;
                     } catch (Exception e) {
                         System.out.println("Error: " + e.getMessage());
                     }
                 }
+                case "logout": {
+                    if (!loggedIn) {
+                        System.out.println(">> You are not logged in. Check the options");
+                        help();
+                        break;
+                    }
+                    try {
+                        facade.logout();
+                        System.out.println(">> You are now logged out. If you need help, type \"help\"");
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                }
+
                 case "list": {
 
                 }
@@ -99,11 +122,6 @@ public class Console {
 
 
         }
-
-
-
-
-
 
     }
 
