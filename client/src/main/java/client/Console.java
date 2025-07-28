af38e2598987bebbbf655963b31cc8dbbed60756
kimpublic <minjoongkim98@naver.com>
2025-07-29 04:17:28 +0900

    working on console, need to test first

--- client/src/main/java/client/Console.java
+++ client/src/main/java/client/Console.java
@@ -1,9 +1,111 @@
 package client;
 
+import ui.EscapeSequences;
+
 import java.util.Scanner;
 
 public class Console {
 
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Console.class);
+    private final ServerFacade facade;
+    private final Scanner scanner = new Scanner(System.in);
 
+    private boolean loggedIn = false;
+    private String cmd = "";
+
+    public Console(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+    public void help() {
+        if (!loggedIn) {
+            System.out.println("Options:");
+            System.out.println("Login as an existing user: \"login\" <USERNAME> <PASSWORD>");
+            System.out.println("Register as a new user: \"register\" <USERNAME> <PASSWORD> <EMAIL>");
+            System.out.println("Exit the program: \"quit\"");
+            System.out.println("Print this action options page: \"help\"");
+        } else {
+            System.out.println("Options:");
+            System.out.println("List current games: \"list\"");
+            System.out.println("Create a new game: \"create\" <GAME NAME>");
+            System.out.println("Join a game: \"join\" <GAME ID> <COLOR TO PLAY: \"black\" or \"white\">");
+            System.out.println("Watch a game: \"watch\" <GAME ID>");
+            System.out.println("Logout: \"logout\"");
+            System.out.println("Exit the program: \"quit\"");
+            System.out.println("Print this action options page: \"help\"");
+        }
+    }
+
+    public void run() {
+        System.out.print(EscapeSequences.ERASE_SCREEN);
+        System.out.println("Welcome to the Chess game! Sign in or log in with your account to start");
+        help();
+
+        while (true) {
+            String line = scanner.nextLine().trim();
+            String[] parsed = line.split(" ", 2);
+            cmd = parsed[0];
+            String arguments = parsed[1];
+            switch (cmd) {
+                case "quit": {
+                    System.out.println("Hope you had a fun time with us. Good bye and see you next time!");
+                    return;
+                }
+                case "help": {
+                    help();
+                    break;
+                }
+                case "register": {
+                    try {
+                        String[] argumentsParsed = arguments.split(" ");
+                        if (argumentsParsed.length < 4) {
+                            System.out.println("Usage: \"register\" <USERNAME> <PASSWORD> <EMAIL>");
+                            break;
+                        }
+                        facade.register(argumentsParsed[1], argumentsParsed[2], argumentsParsed[3]);
+                        loggedIn = true;
+                        System.out.println(">> Registered! You are not logged in.");
+                    } catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                }
+                case "login": {
+                    try {
+                        String[] argumentsParsed = arguments.split(" ");
+                        if (argumentsParsed.length < 3) {
+                            System.out.println("Usage: \"login\" <USERNAME> <PASSWORD>");
+                            break;
+                        }
+                        facade.login(argumentsParsed[1], argumentsParsed[2]);
+                        loggedIn = true;
+                        System.out.println(">> You are now logged in.");
+                    } catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                    }
+                }
+                case "list": {
+
+                }
+                case "create": {
+
+                }
+                case "join": {
+
+                }
+                case "watch": {
+
+                }
+                // 잘못된 커맨드인 경우에도 처리할 수 있도록
+            }
+
+
+        }
+
+
+
+
+
+
+    }
 
 }
